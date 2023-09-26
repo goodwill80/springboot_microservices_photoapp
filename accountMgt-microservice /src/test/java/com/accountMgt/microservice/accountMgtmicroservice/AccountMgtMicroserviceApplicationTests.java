1a8ebc6a6359cf649b37de561e9402e97dc0229e
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-09-26 18:47:44 +0800

    added account microservice

---
+++ accountMgt-microservice /src/test/java/com/accountMgt/microservice/accountMgtmicroservice/AccountMgtMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accountMgt.microservice.accountMgtmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountMgtMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user-microservice /README.md
@@ -0,0 +1,4 @@
+# Setup and user microservice and register with discovery
+1. import spring-cloud-starter-netflix-eureka-client
+2. Annotate @EnableDiscoveryClient in main spring application
+3. Configure the registration in application.properties - refer to file

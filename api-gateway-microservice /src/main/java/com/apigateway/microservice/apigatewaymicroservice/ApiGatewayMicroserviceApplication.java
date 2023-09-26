ec1b5b42e8308be147bae2c58328a017f8445d29
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-09-26 20:37:42 +0800

    added spring cloud api gateway microservice

---
+++ api-gateway-microservice /src/main/java/com/apigateway/microservice/apigatewaymicroservice/ApiGatewayMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.apigateway.microservice.apigatewaymicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayMicroserviceApplication.class, args);
+	}
+
+}

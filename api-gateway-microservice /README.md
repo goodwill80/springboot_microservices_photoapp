b24be1efc3a6f5b734eda420231f86dc5c1f0e85
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-09-28 10:19:37 +0800

    configured spring gateway

---
+++ api-gateway-microservice /README.md
@@ -0,0 +1,5 @@
+# Setup of api gateway with spring cloud
+1. import Gateway, Spring Reactive Web, Eureka Discovery Client
+2. Annotate @EnableDiscoveryClient in main application
+3. Refer to application.properties for configurations
+4. Notes - Spring Gateway has a build-in load balancer
--- api-gateway-microservice /src/main/java/com/apigateway/microservice/apigatewaymicroservice/ApiGatewayMicroserviceApplication.java
+++ api-gateway-microservice /src/main/java/com/apigateway/microservice/apigatewaymicroservice/ApiGatewayMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.apigateway.microservice.apigatewaymicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayMicroserviceApplication {
 
 	public static void main(String[] args) {
